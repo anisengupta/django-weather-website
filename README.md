872764dfdd280eb2e367b93f59dfcc16d25a0161
AniSengupta <aniruddha.sengupta@omd.com>
2021-08-17 17:26:34 +0100

    Changed location of Procfile.

--- README.md
+++
@@ -1,2 +0,0 @@
-# weather_app
-A simple website built using Django that displays the weather for a given city.
--- utils/config.py
+++
@@ -1,4 +0,0 @@
-# This is the config file for the apps in weather_app.py to function
-
-API_KEY = '1245531ca71de473ce8150502d0f2f48'
-CITY_NAME = 'Downham Market'
